cf331e47db489582fe312085e4f57ec48784dd10
kartik12-sudo <kartik.kumar21b@iiitg.ac.in>
2026-02-24 16:09:41 +0530

    Fixed Api calls formatting issues

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "/api",
+  baseURL: "", 
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true, 
 });
 
 api.interceptors.request.use(
